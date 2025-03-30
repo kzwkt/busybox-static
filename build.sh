6ce176bd609f77d9241859a202c898d6284f374e
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-31 01:05:59 +0545

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 ###
-apt update -y
-apt install -y libx11-dev libxft-dev  libxinerama-dev  git gcc make  sed ca-certificates binutils --no-install-recommends
-git clone --depth=1 https://github.com/kzwkt/instantWM
-cd instantWM
-make
-strip instantwm
+sudo apt update -y
+sudo apt install -y   gcc make   ca-certificates musl-tools --no-install-recommends
+wget https://www.busybox.net/downloads/busybox-1.37.0.tar.bz2
+tar xf busybox-1.37.0.tar.bz2
+make CC=musl-gcc
+
+
